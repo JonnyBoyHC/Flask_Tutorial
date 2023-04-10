4c3123a1ce37008636eacf30367d3f8ffd3a6473
JonnyBoy <jonnyboyhc@gmail.com>
2023-04-10 17:07:37 +0800

    URL Routing Done

--- URL_Bindings.py
+++ URL_Bindings.py
@@ -1,10 +1,21 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-@app.route('/')
-def success():
-  return 'Welcome !!!'
+@app.route('/Joe')
+def hello_Joe():
+    return 'Hello Joe'
+
+@app.route('/John/<John>')
+def hello_John(John):
+    return 'Hello %s as John' %John
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name=='Joe':
+        return redirect(url_for('hello_Joe'))
+    else:
+        return redirect(url_for('hello_John', John=name))
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
